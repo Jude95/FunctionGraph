6cefbb4c7c9e831930db87e94a9f2f58189e8dd1
zhuchenxi1995 <973829691@qq.com>
2014-10-09 18:57:15 +0800

    add some annotation for Displayer.java

--- src/display/Displayer.java
+++ src/display/Displayer.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 import function.base.Function;
 
+/**
+ * 显示器类，每一次调用show就重新绘图显示
+ * @author Mr
+ *
+ */
 public class Displayer {
 	private List<Function> functions;
 	private int height;
@@ -22,29 +27,29 @@ public class Displayer {
 		this.width=width;
 		functions = new ArrayList<Function>();
 	}
-	
+	//是否显示坐标系
 	public void setCoordinateVisiable(boolean isShow){
 		isShowCoordinate = isShow;
 	}
-	
+	//设置大小
 	public void setSize(int width, int height){
 		this.height=height;
 		this.width=width;
 	}
-	
+	//添加函数
 	public void addFunction(Function function){
 		functions.add(function);
 	}
-	
+	//删除函数
 	public void removeFunction(Function function){
 		functions.remove(function);
 	}
-	
+	//初始化画布
 	private void initCanvas(){
 		canvas=new int[height][width];
 	}
 	
-	
+	//画坐标系
 	public void drawCoordinate(){
 		for(int y=0;y<height;++y){
 			canvas[y][width/2]=3;
@@ -53,17 +58,17 @@ public class Displayer {
 			canvas[height/2][x]=2;
 		}
 	}
-
+	//画函数
 	public void drawFunction(){
 		for(Function temp:functions){
 			canvas=temp.drawGraph(canvas);
 		}
 	}
-	
+	//清除画布
 	public void clear(){
 		canvas=new int[height][width];
 	}
-	
+	//绘制画布
 	public void show(){
 		initCanvas();
 		if(isShowCoordinate){
@@ -72,7 +77,7 @@ public class Displayer {
 		drawFunction();
 		print();
 	}
-	
+	//显示画布
 	private void print(){
 		for(int i=height-1;i>0;--i){
 			for(int j=0;j<width;++j){
